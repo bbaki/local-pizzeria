5eb25511c5ae9286d9a028ab6df7e2a75c6ee5f6
firaga <agafilam@gmail.com>
2020-01-13 11:44:13 +0100

    first commit

---
+++ desktop/pizza.js
@@ -0,0 +1,67 @@
+ document.addEventListener("DOMContentLoaded", function() {
+ // slider
+ const slider = document.querySelector(".slider");
+    const sliderStage = document.querySelector(".slider-stage");
+    const slides = document.querySelectorAll(".slider li");
+    const prev = document.querySelector(".previous-arrow");
+    const next = document.querySelector(".next-arrow");
+
+    const slideWidth = slides[0].clientWidth;
+    let currentIndex = 0;
+    let slidesNumber = slides.length - 1;
+
+   function goToSlide(index) {
+
+        if (index < 0) {
+            index = slidesNumber;
+        } else if (index > slidesNumber) {
+            index = 0;
+        }
+        
+        slider.style.left = index * (-slideWidth) + "px";
+        currentIndex = index;
+        }
+
+    function slideToNext () {
+       goToSlide(currentIndex + 1);
+    }
+
+    function slideToPrev () {
+       goToSlide(currentIndex - 1);
+    }
+
+    prev.addEventListener("click", slideToPrev);
+    next.addEventListener("click", slideToNext);       
+    setInterval(slideToNext, 4000);
+
+    // read more-less
+    const readMoreLessBtns = document.querySelectorAll(".read-more-less-btn");
+    
+    function showHideText() {
+        const siblingText = this.previousElementSibling;
+
+        if (siblingText.style.display === "none" || siblingText.style.display === "") {
+            siblingText.style.display = "block";
+            this.textContent = "Czytaj mniej";
+        } else {
+            siblingText.style.display = "none";
+            this.textContent = "Czytaj wiecej";
+        }
+    }
+
+    for (let i = 0; i < readMoreLessBtns.length; i++) {
+        readMoreLessBtns[i].addEventListener("click", showHideText);
+    }
+
+    // go-up button
+
+    const goUpBtn = document.querySelector(".go-up i");
+
+    goUpBtn.addEventListener("click", function() {
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+    });
+});
